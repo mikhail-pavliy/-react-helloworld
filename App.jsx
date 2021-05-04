fd4ab4d05f92cb28f788fd7afdc107c6034ace2f
mikhail-pavliy <69155591+mikhail-pavliy@users.noreply.github.com>
2021-05-04 22:24:26 +0600

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>Hello, Mikhail.Pavliy and Otus from react!!!</div>
+    );
+  }
+}
+
+export default App;
